cbdbd78f4f467036e15e09434c764c5cfa6ce79f
Vansi Suryaprakash Mishra <vansisurya123@gmail.com>
2024-10-05 12:07:35 +0530

    Initial Commit

---
+++ sneakspeak/src/models/index.js
@@ -0,0 +1,60 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface Message extends Document {
+    content: string;  //string is small in typescript
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,    //String is capital in Mongoose
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,   
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;  //string is small in typescript
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    message: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, 'please use a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code expiry is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        required: [true, ]
+    }
+})
